c4290cc7803a063fb22174bfd2269929ef3b8c1c
Ryan Kliemann <Ryan_blake@icloud.com>
2016-05-13 17:44:49 -0500

    collection controller

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,43 @@
+var albumPicasso = {
+  title: 'The cool',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/03.png',
+  songs: [
+    { title: 'Blue', duration: 161.71, audioUrl: '/assets/music/blue' },
+    { title: 'Green', duration: 103.96, audioUrl: '/assets/music/green' },
+    { title: 'Red', duration: 268.45, audioUrl: '/assets/music/red' },
+    { title: 'Pink', duration: 153.14, audioUrl: '/assets/music/pink' },
+    { title: 'Magenta', duration: 374.22, audioUrl: '/assets/music/magenta' }
+     ]
+};
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [
+    { title: 'Hello, Operator?', duration: '1:01' },
+    { title: 'Ring, ring, ring', duration: '5:01' },
+    { title: 'Fits in your pocket', duration: '3:21'},
+    { title: 'Can you hear me now?', duration: '3:14' },
+    { title: 'Wrong phone number', duration: '2:15'}
+  ]
+};
+
+var albumRyan = {
+  title: 'The microphone',
+  artist: 'Parconi',
+  label: 'me',
+  year: '1990',
+  albumArtUrl: 'assets/images/album_covers/05.png',
+  songs: [
+    { title: 'Hello, sunshine?', duration: '1:01' },
+    { title: 'Ring, rang, ring', duration: '5:01' },
+    { title: 'Fits in no pockets', duration: '3:21'},
+    { title: 'Can we hear me us?', duration: '3:14' },
+    { title: 'Wrong tele number', duration: '2:15'}
+  ]
+};
