312b5bee3c2e44e0af9204b30280974aad870b8c
Ryan Kliemann <Ryan_blake@icloud.com>
2016-05-23 09:46:30 -0500

    player bar play pause previous

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,7 +1,14 @@
 (function() {
-  function SongPlayer() {
+  function SongPlayer(Fixtures) {
     var SongPlayer = {};
-    var currentSong = null;
+    var currentAlbum = Fixtures.getAlbum();
+
+
+    var getSongIndex = function(song) {
+       return currentAlbum.songs.indexOf(song);
+    };
+
+    SongPlayer.currentSong = null;
 
 /**
 * @desc Buzz object audio file
@@ -17,7 +24,7 @@
 var setSong = function(song) {
     if (currentBuzzObject) {
         currentBuzzObject.stop();
-        currentSong.playing = null;
+        SongPlayer.currentSong = null;
     }
 
     currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -25,20 +32,21 @@ var setSong = function(song) {
       preload: true
     });
 
-   currentSong = song;
+   SongPlayer.currentSong = song;
 };
 
   var playSong = function(song) {
     currentBuzzObject.play();
     song.playing = true;
-  }
+  };
 
  SongPlayer.play = function(song) {
-   if (currentSong !== song) {
+   song = song || SongPlayer.currentSong;
+   if (SongPlayer.currentSong !== song) {
        setSong(song);
        playSong(song);
 
-     } else if (currentSong === song) {
+     } else if (SongPlayer.currentSong === song) {
          if (currentBuzzObject.isPaused()) {
           playSong(song);
          }
@@ -46,9 +54,24 @@ var setSong = function(song) {
  };
 
  SongPlayer.pause = function(song) {
+     song = song || SongPlayer.currentSong;
      currentBuzzObject.pause();
      song.playing = false;
  };
+
+ SongPlayer.previous = function() {
+    var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+    currentSongIndex--;
+
+    if (currentSongIndex < 0) {
+      currentBuzzObject.stop();
+      SongPlayer.currentSong.playing = null;
+    } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+    }
+};
 
  return SongPlayer;
 }
